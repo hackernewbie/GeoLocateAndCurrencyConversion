47bcc68154aa5532a331cd797a7383a23a8a124e
Rajiv Verma <rajiv@webguy.in>
2021-09-24 13:31:56 +0530

    made text field required

--- resources/views/geolocate.blade.php
+++ resources/views/geolocate.blade.php
@@ -18,7 +18,7 @@
 
         <form action="{{route('convert') }}" method="POST">
             @csrf
-            Converting <input type="text" name="enteredvalue"/> To
+            Converting <input type="text" name="enteredvalue" required/> To
             <select name="convertToCurr" id="convertToCurr" class="convertToCurr">
                 <option value="USD">USD</option>
                 <option value="GBP">GBP</option>
